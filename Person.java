ca14cbb21b4d20436cecbbbd866ca4b9910ace84
Surface <ez9573@gmail.com>
2021-01-09 13:41:17 -0800

    hi

--- Person.java
+++ Person.java
@@ -4,23 +4,27 @@ public class Person{
     private String FirstName;
     private String LastName;
     private int grade;
+    private int age;
     private Map<String, Integer> coursesTaken;
     private String school;
     private String[] coursesInterested;
     private String[] subjectsInterested;
     private boolean transcriptVerified;
+    private double rating;
 
-    public Person(String FirstName, String LastName, int grade, Map<String, Integer> coursesTaken, String school,
-                  String[] coursesInterested, String[] subjectsInterested, boolean transcriptVerified) {
+    public Person(String FirstName, String LastName, int grade, int age, Map<String, Integer> coursesTaken, String school,
+                  String[] coursesInterested, String[] subjectsInterested, boolean transcriptVerified, double rating) {
         this.FirstName = FirstName;
         this.LastName = LastName;
         this.grade = grade;
+        this.age = age;
         this.coursesInterested = coursesInterested;
         this.coursesTaken = coursesTaken;
         this.school = school;
         this.coursesInterested = coursesInterested;
         this.subjectsInterested = subjectsInterested;
         this.transcriptVerified = transcriptVerified;
+        this.rating = rating;
     }
 
     public Map<String, Integer> getCoursesTaken() {
@@ -37,6 +41,10 @@ public class Person{
 
     public boolean isValid() {
         return transcriptVerified;
+    }
+
+    public boolean eval() {
+        return (rating <= 2.5);
     }
 
 }
