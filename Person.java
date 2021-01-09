09979d4898260a3eec45243f37c6bdaa41cdd7f2
Surface <ez9573@gmail.com>
2021-01-09 13:30:00 -0800

    message

--- Person.java
+++ Person.java
@@ -1,28 +1,36 @@
 public class Person{
     private String FirstName;
     private String LastName;
-    private String[] coursesTaken;
+    private int grade;
+    private Map<String, int> coursesTaken;
     private String school;
     private String[] coursesInterested;
+    private String[] subjectsInterested;
     private boolean transcriptVerified;
 
-    public Person(String FirstName, String LastName, String[] coursesTaken, String school,
-                  String[] coursesInterested, boolean transcriptVerified) {
+    public Person(String FirstName, String LastName, int grade, Map<String, int> coursesTaken, String school,
+                  String[] coursesInterested, String[] subjectsInterested, boolean transcriptVerified) {
         this.FirstName = FirstName;
         this.LastName = LastName;
+        this.grade = grade;
         this.coursesInterested = coursesInterested;
         this.coursesTaken = coursesTaken;
         this.school = school;
         this.coursesInterested = coursesInterested;
+        this.subjectsInterested = subjectsInterested;
         this.transcriptVerified = transcriptVerified;
     }
 
-    public String[] getCoursesTaken() {
+    public Map<String, int> getCoursesTaken() {
         return this.coursesTaken;
     }
 
     public String[] getCoursesInterested() {
         return this.coursesInterested;
+    }
+
+    public String[] getSubjectsInterested() {
+        return this.getSubjectsInterested;
     }
 
     public boolean isValid() {
